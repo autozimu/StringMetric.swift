bde98f3086304a97ca60c3b843b68d1055833e82
Jeff Li <junfengl@ca.ibm.com>
2016-06-27 09:21:11 -0400

    Add unicode test for DamerauLevenshteinDistance.

--- Tests/StringMetric/StringMetricTests.swift
+++ Tests/StringMetric/StringMetricTests.swift
@@ -6,9 +6,7 @@ class StringMetricTests: XCTestCase {
         XCTAssertEqual("".LevenshteinDistance(between: "sitting"), 7)
         XCTAssertEqual("kitten".LevenshteinDistance(between: ""), 6)
         XCTAssertEqual("kitten".LevenshteinDistance(between: "sitting"), 3)
-    }
 
-    func testLevenshteinDistanceOfUnicode() {
         XCTAssertEqual("君子和而不同".LevenshteinDistance(between: "小人同而不和"), 4)
     }
 
@@ -18,14 +16,14 @@ class StringMetricTests: XCTestCase {
         XCTAssertEqual("kitten".DamerauLevenshteinDistance(between: "sitting"), 3)
 
         XCTAssertEqual("CA".DamerauLevenshteinDistance(between: "AC"), 1)
+        XCTAssertEqual("specter".DamerauLevenshteinDistance(between: "spectre"), 1)
         XCTAssertEqual("CA".DamerauLevenshteinDistance(between: "ABC"), 2)
 
-        XCTAssertEqual("specter".DamerauLevenshteinDistance(between: "spectre"), 1)
+        XCTAssertEqual("君子和而不同".DamerauLevenshteinDistance(between: "小人同而不和"), 4)
     }
 
     static let allTests = [
         ("testLevenshteinDistance", testLevenshteinDistance),
-        ("testLevenshteinDistanceOfUnicode", testLevenshteinDistanceOfUnicode),
         ("testDamerauLevenshteinDistance", testDamerauLevenshteinDistance),
     ]
 }
