ba54c8ddc6b277a2bf2b641c7a2358cb48f4d38a
BAN Jun <banjun@gmail.com>
2018-11-26 12:42:42 +0900

    migrate to Swift 4.2 package manager description and fix build errors

--- Package.swift
+++ Package.swift
@@ -1,5 +1,10 @@
+// swift-tools-version:4.2
 import PackageDescription
 
 let package = Package(
-    name: "StringMetric"
+    name: "StringMetric",
+    targets: [
+        .target(name: "StringMetric", path: "Sources"),
+        .testTarget(name: "Tests", dependencies: ["StringMetric"], path: "Tests")
+    ]
 )
