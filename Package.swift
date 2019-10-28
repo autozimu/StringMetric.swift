941d28d2ea0622be498027ea0bc97db472f0251c
Jeroen Besse <jeroen.b.k@hotmail.com>
2019-10-28 16:50:14 +0100

    Add products to Package.swift file to support Swift 5 Package Manager

--- Package.swift
+++ Package.swift
@@ -3,6 +3,9 @@ import PackageDescription
 
 let package = Package(
     name: "StringMetric",
+    products: [
+        .library(name: "StringMetric", targets: ["StringMetric"]),
+    ],
     targets: [
         .target(name: "StringMetric", path: "Sources"),
         .testTarget(name: "StringMetricTests", dependencies: ["StringMetric"], path: "Tests/StringMetricTests")
